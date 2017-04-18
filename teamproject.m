92e38ff5c6aa1c658cf2e5aa7a3a113b3d33f552
sparra12 <s.parra12@uniandes.edu.co>
2017-04-17 21:02:17 -0500

    Ahí va lo del Plot exams
    Espero que esté bien

--- teamproject.m
+++ teamproject.m
@@ -225,6 +225,107 @@ function plotExams_Callback(hObject, eventdata, handles)
 % hObject    handle to plotExams (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
+%Abrir archivo con los datos de los ex?menes
+nombre1 = handles.paciente(handles.actual).Nombre;
+nombre1 = strrep(nombre1,' ', '_');
+nombre1 = strrep(nombre1,',', '');
+fid = fopen(nombre1);
+%escaneo del archivo txt
+M2 = textscan(fid , '%s', 6, 'Delimiter', '\t','MultipleDelimsAsOne',1);
+contador = 0;
+i = 1;
+%Crear el array de cells con los datos
+while ~isempty(M2{i});
+    i = i + 1;
+    A = textscan(fid, '%s',6, 'Delimiter', '\t','MultipleDelimsAsOne',1);
+    M2{i} = A{1};
+    contador = contador + 1;
+end
+ 
+%Asignar los datos de cada categoria a un arreglo de strings
+Dates = strings(1,contador-1);
+for i=1:contador-1
+y = M2{i+1}{1};
+Dates(1,i) = y;
+end
+ 
+Temp = strings(1,contador-1);
+for i=1:contador-1
+y = M2{i+1}{3};
+Temp(1,i) = y;
+end
+ 
+Plow = strings(1,contador-1);
+for i=1:contador-1
+y = M2{i+1}{4};
+Plow(1,i) = y;
+end
+ 
+Phigh = strings(1,contador-1);
+for i=1:contador-1
+y = M2{i+1}{5};
+Phigh(1,i) = y;
+end
+ 
+Beats = strings(1,contador-1);
+for i=1:contador-1
+y = M2{i+1}{6};
+Beats(1,i) = y;
+end
+ 
+%Llamar los arreglos de strings con los datos
+numeroBra = 0;
+numeroTac = 0;
+
+figure
+subFigure1 = subplot(3,1,1);
+x1 = (1:size(Temp));
+for i = 1:size(Temp);
+    y1(i) = Temp(i);
+end
+plot(subFigure1,x1,y1,'y')
+xlabel('Date')
+ylabel('Temperature (?C)')
+
+subFigure2 = subplot(3,1,2);
+x1 = (1:size(Phigh));
+for i = 1:size(Phigh);
+    y1(i) = Phigh(i);
+end
+for i = 1:size(Plow);
+    y2(i) = Plow(i);
+end
+for i = 1:size(Plow);
+    y3(i) = ((Plow(i)+Phigh(i))/2); 
+end
+plot(subFigure2,x1,y1,'r',x1,y2,'r',x1,y3,'y')
+xlabel('Date')
+ylabel('Pressure (mmHg)')
+
+subFigure3 = subplot(3,1,3);
+x1 = (1:size(Beats));
+for i = 1:size(Beats);
+    y1(i) = Beats(i);
+end
+plot(subFigure3,x1,y1,'y')
+xlabel('Date')
+ylabel('Heart Rate (bpm)')
+
+for i = 1:size(Beats);
+    if Beats(i)<60
+        numeroBra = numeroBra + 1;
+    elseif Beats(i)>100
+        numeroTac = numeroTac + 1;
+    end
+end
+
+if numeroBra>0 && numeroTac>0
+    msgbox(['The patient has ', num2str(numeroBra), ' alerts as a bradychardic patient, and ', num2str(numeroTac)] , ' alerts as a tachycardic patient.','Alert')
+elseif numeroBra>0
+    msgbox(['The patient has ', num2str(numeroBra), ' alerts as a bradychardic patient.'],'Alert');
+elseif numeroTac>0
+    msgbox(['The patient has ', num2str(numeroTac), ' alerts as a tachycardic patient.'],'Alert');
+end
 
 
 
